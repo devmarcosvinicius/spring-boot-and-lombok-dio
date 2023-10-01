daa2b067e133e9d9473ff8208b036e45b851d566
Marcos Vinicius <contato@marcosviniciusdev.com>
2023-10-01 17:25:08 -0300

    clientRepository and his constructor was added. Method to listAll created to list all clients using findAll from repository.

--- src/main/java/com/marcosviniciusdev/controller/ClientController.java
+++ src/main/java/com/marcosviniciusdev/controller/ClientController.java
@@ -1,5 +1,25 @@
 package com.marcosviniciusdev.controller;
 
+import com.marcosviniciusdev.model.ClientModel;
+import com.marcosviniciusdev.repository.ClientRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/client")
 public class ClientController {
+
+    private final ClientRepository clientRepository;
+
+    public ClientController(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
+
+    public ResponseEntity<List<ClientModel>> listAll() {
+        return ResponseEntity.ok(clientRepository.findAll());
+    }
+
 }
