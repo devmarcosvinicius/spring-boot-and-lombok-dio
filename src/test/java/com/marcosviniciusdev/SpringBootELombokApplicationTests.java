0934b0c9b3bc817b552d98a3e82bb4d4d743a493
Marcos Vinicius <contato@marcosviniciusdev.com>
2023-10-01 17:02:03 -0300

    First commit

---
+++ src/test/java/com/marcosviniciusdev/SpringBootELombokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcosviniciusdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootELombokApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
