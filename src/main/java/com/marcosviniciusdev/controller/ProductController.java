ffac895ea7313a717af0ae18a9cdea780988941c
Marcos Vinicius <contato@marcosviniciusdev.com>
2023-10-02 01:18:00 -0300

    changing the request mapping path.

--- src/main/java/com/marcosviniciusdev/controller/ProductController.java
+++ src/main/java/com/marcosviniciusdev/controller/ProductController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/client")
+@RequestMapping("/api/product")
 public class ProductController {
 
     private final ProductRepository productRepository;
