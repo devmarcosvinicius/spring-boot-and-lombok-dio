efae945e2c95bcf2e7f34c8385a928c15cb56c2f
Marcos Vinicius <contato@marcosviniciusdev.com>
2023-10-02 01:17:18 -0300

    Fixing some bugs.

--- src/main/java/com/marcosviniciusdev/model/ProductModel.java
+++ src/main/java/com/marcosviniciusdev/model/ProductModel.java
@@ -1,16 +1,50 @@
 package com.marcosviniciusdev.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 import java.math.BigDecimal;
 
-@Entity(name = "produto")
+@Entity(name = "produts")
 public class ProductModel {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String description;
-    private BigDecimal valor;
+    private BigDecimal value;
 
+    public ProductModel() {}
+
+    public ProductModel(Long id, String description, BigDecimal value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    public void setValue(BigDecimal value) {
+        this.value = value;
+    }
 }
