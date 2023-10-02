39d51fa0661d825277168301cc91dc092a365a36
Marcos Vinicius <contato@marcosviniciusdev.com>
2023-10-02 00:44:45 -0300

    ProductModel created.

--- src/main/java/com/marcosviniciusdev/model/ProductModel.java
+++ src/main/java/com/marcosviniciusdev/model/ProductModel.java
@@ -1,4 +1,16 @@
 package com.marcosviniciusdev.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+import java.math.BigDecimal;
+
+@Entity(name = "produto")
 public class ProductModel {
+
+    @Id
+    private Long id;
+    private String description;
+    private BigDecimal valor;
+
 }
