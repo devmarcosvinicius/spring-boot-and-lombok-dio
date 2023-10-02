a5aed12d71c65cedd260e9afcb36a1b62612e515
Marcos Vinicius <contato@marcosviniciusdev.com>
2023-10-02 11:28:30 -0300

    Lombok implementation.

--- src/main/java/com/marcosviniciusdev/controller/ClientController.java
+++ src/main/java/com/marcosviniciusdev/controller/ClientController.java
@@ -2,6 +2,7 @@ package com.marcosviniciusdev.controller;
 
 import com.marcosviniciusdev.model.ClientModel;
 import com.marcosviniciusdev.repository.ClientRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,13 +11,10 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/client")
+@AllArgsConstructor
 public class ClientController {
 
     private final ClientRepository clientRepository;
-
-    public ClientController(ClientRepository clientRepository) {
-        this.clientRepository = clientRepository;
-    }
 
     @GetMapping("/listall")
     public ResponseEntity<List<ClientModel>> listAll() {
